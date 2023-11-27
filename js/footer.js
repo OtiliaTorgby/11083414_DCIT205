c76bdbdbb159300193725f5bb722d7bb8a782416
OtiliaTorgby <otiliatorgby@gmail.com>
2023-11-27 16:09:23 +0000

    Added footer page

---
+++ js/footer.js
@@ -0,0 +1,67 @@
+const footerHTML = `  <footer>
+
+      <div class="footer-col">
+        <h3>Academics</h3>
+        <li>Graduate Courses(MPhil Computer Science)</li>
+        <li>Graduate Courses(MSc Computer Science)</li>
+        <li>Graduate Courses(PhD)</li>
+        <li>Academic Calendar</li>
+        <li>Level 200 & 300 Timetable</li>
+        <li>Placements For Level 200 Students</li>
+        <li>Student Complaint Form</li>
+        </div>
+
+      <div class="footer-col">
+        <h3>Admissions</h3>
+        <li>Undergraduate</li>
+        <li>Graduate</li>
+        <li>International Students</li>
+        <li>Entry Requirements</li>
+        <li>Contact Us</li>
+      </div>
+
+      <div class="footer-col">
+        <h3>Research</h3>
+        <li>Research at DCS</li>
+        <li>Research Impact</li>
+        <li>Libraries</li>
+      </div>
+      
+      <div class="footer-col">
+        <h3>About The Department</h3>
+        <li>Introducing DCS</li>
+        <li>Head of Department's office</li>
+        <li>Faculty</li>
+        <li>Events</li>
+      </div>
+
+        <div class="footer-col">
+        <h3>Contact Us</h3>
+        <p>Phone & Email</p>
+        <li><i class="fa fa-phone" aria-hidden="true"></i> +233 501 382 035</li>
+        <li><i class="fa fa-phone" aria-hidden="true"></i> +233 559 145 698</li>
+        <li><i class="fa fa-envelope" aria-hidden="true"></i>dcs@ug.edu.gh</li>
+      </div>
+
+      <div class="footer-col">
+      <h3>Location</h3>
+      <li>University Of Ghana,Legon</li>
+      <li>Campus</li>
+      <li>Opposite Mathematics</li>
+      <li>Department</li>
+      </div>
+
+      <div class="copyright">
+      <p>Copyright ©2023 All rights reserved</p>
+      <div class="pro-links">
+      <a href="https://www.instagram.com/explore/locations/468119891/university-of-ghana/">
+      <i class="fab fa-facebook-f"></i></a>
+      <a href="https://twitter.com/univofghana">
+      <i class="fab fa-twitter"></i></a>
+      <a href="https://www.instagram.com/explore/locations/468119891/university-of-ghana/">
+      <i class="fab fa-instagram"></i></a>
+      </div>
+      </a>
+      </footer> `;
+
+document.body.insertAdjacentHTML("beforeend", footerHTML);
